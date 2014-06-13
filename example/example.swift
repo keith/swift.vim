79390292acb1ce181c3d126916bae3c636eb8fb9
Keith Smiley <keithbsmiley@gmail.com>
2014-06-13 11:45:22 -0700

    Add an example script

---
+++ example/example.swift
@@ -0,0 +1,23 @@
+#!/Applications/Xcode6-Beta.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/swift -i
+
+// This is a comment
+
+let foo = 5 // another comment
+
+/* this is also a comment */
+
+/* this is
+a multi-line
+comment
+*/
+
+"this is a string"
+"this is a string\" with an escaped quote"
+
+// TODO: This is a todo comment
+// XXX: This is another todo comment
+// FIXME: this is another todo comment
+// NOTE: this is another todo comment
+/* TODO multiple */
+
+// MARK: this is a marker
